9d50a45e11456cf2effb2199fd40f7e060cfd633
marco <marco@localhost.localdomain>
2021-05-01 20:40:15 +0200

    added new encoder

--- str2char.py
+++ str2char.py
@@ -1,11 +1,39 @@
 #!/usr/bin/env python3
 import sys;
 
-if (len(sys.argv) != 2):
-    sys.exit('Usage: {} string-to-convert'.format(sys.argv[0]));
+def showUsage():
+    sys.exit('Usage:\n'
+             '{} format string-to-converti\n\n'
+             'format:\n'
+             '-u urlencode\n'
+             '-j javascript CharCode'.format(sys.argv[0]));
 
-result = ""
-for c in sys.argv[1]:
-    result+=str(hex(ord(c))).replace('0x','%');
+def urlencode(input):
+    result = ""
+    for c in input:
+        result+=str(hex(ord(c))).replace('0x','%');
+    return result
 
-print(result)
+def javascriptCharCode(input):
+    result = ""
+    for c in input:
+        result+=str(hex(ord(c)))+',';
+    return result[:-1]
+
+
+if (len(sys.argv) != 3):
+    showUsage()
+
+if (sys.argv[1] == '-u'):
+    print(urlencode(sys.argv[2]))
+    exit()
+
+
+if (sys.argv[1] == '-j'):
+    print(javascriptCharCode(sys.argv[2]))
+    exit()
+
+if (sys.argv[1] == '-h'):
+    showUsage()
+
+showUsage()
